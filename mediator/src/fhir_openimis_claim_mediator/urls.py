f9ac31849971d0dae71c47da5a36083f46773236
Bagaja <bagajab@gmail.com>
2025-07-29 18:27:24 +0300

    Yes

--- mediator/src/fhir_openimis_claim_mediator/urls.py
+++
@@ -1,7 +0,0 @@
-# src/fhir_openimis_claim_mediator/urls.py
-from django.urls import path
-from .views import fhir_claim_mediator_view
-
-urlpatterns = [
-    path('claim/', fhir_claim_mediator_view, name='claim_mediator'),
-]
